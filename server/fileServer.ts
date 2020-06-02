e2de8e748bf83a5a4f780fb94e1c15f529616806
Leon Talbert <leon.talbert@gmail.com>
2020-06-02 16:18:37 +0800

    redirect to index

--- server/fileServer.ts
+++ server/fileServer.ts
@@ -4,7 +4,7 @@ import {Express} from "express";
 
 const fileServer = (app: Express) => {
     app.use(express.static(path.join(__dirname, '../build')));
-    app.get('/', function (req, res) {
+    app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, '../build', 'index.html'));
     });
 };
