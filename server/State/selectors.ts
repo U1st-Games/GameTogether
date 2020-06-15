de35db220b8c35181f016af5492f0c9e7f950c5e
Leon Talbert <leon.talbert@gmail.com>
2020-06-15 11:18:02 +0800

    update server redux state code

--- server/State/selectors.ts
+++ server/State/selectors.ts
@@ -1,9 +1,9 @@
-import store, { State } from './state';
+import store, {Room, State} from './state';
 
 export const selectRoomById = (roomId: string) =>
-    store.getState().rooms.find(room => room.roomId == roomId);
+    store.getState().rooms.find((room: Room) => room.roomId == roomId);
 
 export const isHost = (socketId: string) =>{
-    return !!store.getState().rooms.find(room => room.hostSocketId === socketId);
+    return !!store.getState().rooms.find((room: Room) => room.hostSocketId === socketId);
 }
 
