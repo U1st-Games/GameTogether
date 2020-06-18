685c5b5c88f0c6862f6ab86a2a1a097282639b6a
Leon Talbert <leon.talbert@gmail.com>
2020-06-18 15:05:44 +0800

    env var authToken

--- server/stunTurnTokenManager.ts
+++ server/stunTurnTokenManager.ts
@@ -1,7 +1,7 @@
 import {Express} from "express";
 
 const accountSid = 'AC2e112f147e6283be9839c47a0cf7598d';
-const authToken = '28c44ba5b932cb06e4bb97ae3282418a';
+const authToken = process.env.authToken || '';
 const client = require('twilio')(accountSid, authToken);
 
 const stunTurnTokenManager = (app: Express) => {
