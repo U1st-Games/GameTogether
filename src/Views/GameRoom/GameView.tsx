901d79488d4395225904184564a795382085fcda
Leon Talbert <leon.talbert@gmail.com>
2020-06-25 16:19:56 +0800

    remote video styling

--- src/Views/GameRoom/GameView.tsx
+++ src/Views/GameRoom/GameView.tsx
@@ -17,9 +17,16 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import React from 'react';
 import {useParams, Prompt} from "react-router-dom";
+import styled from "styled-components";
 
 import useWebRTCCanvasShare from "../../Hooks/UseWebRTCCanvasShare/useWebRTCCanvasShare";
 import {GameInfo, getCanvasIdByLink} from "../../Shared";
+
+const RemoteVideo = styled.video`
+    width: 100%;
+    height: 100%;
+    background: black;
+`;
 
 const GameView = () => {
     const {gamename, roomid} = useParams();
@@ -46,7 +53,7 @@ const GameView = () => {
                 src={`/${gamename}/index.html`} width={"100%"} height={"100%"}
                 style={{display: isGuest ? 'none' : 'initial', border: 'none'}}
             />
-            <video id={"remoteVideo"} autoPlay muted playsInline/>
+            <RemoteVideo id={"remoteVideo"} autoPlay muted playsInline/>
         </>
     );
 };
