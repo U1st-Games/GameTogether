5b1cea5bc9f22b92f10610bf844a24204d1958dd
Leon Talbert <leon.talbert@gmail.com>
2020-07-08 13:57:35 +0800

    strong etag

--- server/fileServer.ts
+++ server/fileServer.ts
@@ -3,6 +3,7 @@ import path from "path";
 import {Express} from "express";
 
 const fileServer = (app: Express) => {
+    app.set('etag', 'strong')
     app.use(express.static(path.join(__dirname, '../build')));
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, '../build', 'index.html'));
